3c298a0c526e79a1d3aa65d10ee2867bce34677f
Faizan shaikh <faizan@Faizans-MacBook-Air.local>
2026-01-26 01:00:41 +0530

    add trappingrainWater

---
+++ TrappingRainWater/TrapingRainWater.js
@@ -0,0 +1,17 @@
+var trap = function(height) {
+let trapWater=0
+let l=0,r=height.length-1;
+
+
+//step 1 make two pointer and try to find  area of  l to r 
+//stpe 2 minus the water  because of hurdle  values from total
+// in the last return that 
+while(l<r){
+    
+}
+return trapWater;
+};
+
+
+result=trap([4,2,0,3,2,5]);
+console.log(result)

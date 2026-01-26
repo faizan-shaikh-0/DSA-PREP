6ad8d20d45ac0f4c5ff2245e934de7378cab4e15
faizan shaikh <fs61faizan@gmail.com>
2026-01-26 15:25:26 +0530

    trapping water resolve

--- TrappingRainWater/TrapingRainWater.js
+++ TrappingRainWater/TrapingRainWater.js
@@ -1,17 +1,24 @@
 var trap = function(height) {
-let trapWater=0
+let trappedWater=0
 let l=0,r=height.length-1;
+let lmax=0,rmax=0;
 
-
-//step 1 make two pointer and try to find  area of  l to r 
-//stpe 2 minus the water  because of hurdle  values from total
-// in the last return that 
 while(l<r){
+if(height[l]<height[r]){
+    lmax=Math.max(lmax,height[l])
+    trappedWater += lmax - height[l];
+    l++;
+}else{
+         rmax=Math.max(rmax,height[r])
+          trappedWater += rmax - height[r];
+           r--;
+}
     
 }
-return trapWater;
+return trappedWater;
 };
 
 
-result=trap([4,2,0,3,2,5]);
+result=trap([0,1,0,2,1,0,1,3,2,1,2,1]);
 console.log(result)
+
